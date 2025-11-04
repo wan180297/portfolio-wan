6c2a30bfa3c9806ca4afb95915970a04344518a7
wan180297 <wan180297@gmail.com>
2025-11-04 11:52:24 -0300

    links-projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,9 +2,9 @@ export const Projects = () => {
     return(
         <section id="projects">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-6">
-                <a href=""><img src="/assets/piza.png" alt="" /></a>
-                <a href=""><img src="/assets/starbu.png" alt="" /></a>
-                <a href=""><img src="/assets/filmesreact.png" alt="" /></a>
+                <a href="https://github.com/wan180297/projeto-pizzaria"><img src="/assets/piza.png" alt="" /></a>
+                <a href="https://github.com/wan180297/projeto-starbucks"><img src="/assets/starbu.png" alt="" /></a>
+                <a href="https://github.com/wan180297/movies-with-react"><img src="/assets/filmesreact.png" alt="" /></a>
                 <a href="https://github.com/wan180297/conversor-com-js"><img src="/assets/conversor.png" alt="" /></a>
             </div>
         </section>
